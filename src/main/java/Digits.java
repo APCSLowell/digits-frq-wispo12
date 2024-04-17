7d362bcbf50a4bc06d751337be4f5029742894d5
wispo12 <143028172+wispo12@users.noreply.github.com>
2024-04-16 22:40:40 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -23,7 +23,7 @@ digitList.add(0,new Integer(num%10));
 	{ /* to be implemented in part (b) */
 		for(int i=0;i<digitList.size()-1;i++)
 			{
-				if(digitList.get(i)>digitList.get(i+1)){return false;}
+				if(digitList.get(i)>=digitList.get(i+1)){return false;}
 
 	}
 		return true;
