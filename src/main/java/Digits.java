d2e67ed10f18ba77429c35462cba4b05fccd4852
wispo12 <143028172+wispo12@users.noreply.github.com>
2024-04-16 22:18:09 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,15 +7,21 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	for(char ch : num.toCharArray()) { 
+		
+    digitList.add( Integer.valueOf(String.valueOf(ch))); 
+} 
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
+		for(int i=0;i<digitList.size()-1;i++)
+			{
+				if(digitList.get(i)>digitList.get(i+1)){return false;}
 
 	}
-	
+		return true;
+	}
 	public String toString()
 	{
 		return digitList.toString();
